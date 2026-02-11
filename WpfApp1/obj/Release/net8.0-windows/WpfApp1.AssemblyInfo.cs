cc9abefd41064a4b8cd86ab78a9cc3335b1b000e
xanto-dev <elhadjboubacarbah210@gmail.com>
2026-02-11 11:21:31 -0500

    plus flou

--- WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb368a247fa7b17f161d61a469e44ee02ec70e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
